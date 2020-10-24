e249e4bc978cbc38a6a3c52cdbbee5fa0b0db159
MasudSarker <mict.mbstu@gmail.com>
2020-10-24 12:34:05 +0600

    Add files via upload

--- Coding/dataanalysis.py
+++ Coding/dataanalysis.py
@@ -18,13 +18,17 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_curve,roc_auc_score, precision_recall_curve, average_precision_score
 from sklearn.linear_model import LogisticRegression
 
+## import row dataset
 import pandas as pd
-df = pd.read_csv("QueryResults.csv")
+df = pd.read_csv("RowDataSetTest.csv")
+
+## display dataset header to check imported dataset are okay
 df.head()
 
 print(df.head())
 
-## Common and Uncommon problem class ratio 
+## divide dataset into two class (Common and Uncommon error class) and varify dataset ratio
+
 df = df['Class'].value_counts()
 print('Class 0:', df[0])
 print('Class 1:', df[1])
@@ -33,7 +37,6 @@ print('Proportion:', round(df[0] / df[1], 2), ': 1')
 ## Visual view of common and uncommon class 
 df.plot(kind='bar', title='Count (target)');
 
-## Remove lower important feature
 
 
 
